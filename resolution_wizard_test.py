360a3973a6543142b85e766add5d47f48c0b01c5
Yves Adler <yves@confusedbits.net>
2013-12-30 10:55:58 +0100

    remove test package as its a pain with python to go that way

---
+++ resolution_wizard_test.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest_data_provider import data_provider
+from resolution_wizard import ResolutionWizard
+
+class ResolutionWizardTest(unittest.TestCase):
+    displays = lambda: (
+        ( 'LVDS1', ['DP1', 'LVDS1']   ),
+        ( 'LVDS1', ['HDMI1', 'LVDS1'] ),
+        ( 'LVDS1', ['LVDS2', 'LVDS1'] ),
+        ( 'LVDS2', ['LVDS2', 'DP1'] ),
+        ( 'DP1'  , ['HDMI1', 'DP1']   )
+    )
+
+    @data_provider(displays)
+    def test_guess_primary(self, expected, display_list):
+        result = ResolutionWizard(display_list).guess_primary()
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
